2295e79e73bc227d2be2dd3aec2762d6e8cc9d56
Cqptomii <tomfraisse.pro@gmail.com>
2023-11-25 12:41:59 +0100

    Udpate Files

--- mesure_capteurs.h
+++ mesure_capteurs.h
@@ -4,5 +4,11 @@
 
 #ifndef PROJETIF3B_MESUR_CAPTEURS_H
 #define PROJETIF3B_MESUR_CAPTEURS_H
-
+#include <OLED_I2C.h>
+#include <Wire.h>
+#include <math.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_CCS811.h>
+void UpdateOLED();
 #endif //PROJETIF3B_MESUR_CAPTEURS_H
--- template_projet.ino
+++ template_projet.ino
@@ -1,34 +1,37 @@
 #include <WiFi.h> // Cette bibliothèque permet à l'ESP32 de se connecter au réseau WiFi.
 #include <PubSubClient.h> // Cette bibliothèque vous permet d'envoyer et de recevoir des messages MQTT.
-/*
+#include "mesure_capteurs.ino"
+#define EARTHPRESSUR (1013.25)
 
-  Declaration des variables liés aux pin des capteurs et actionneurs + variables d'états
-
-const int ledPin = 33; // Pin de la LED
-const int buttonPin = 32; // Pin du bouton
-int buttonState = 0; 
-*/
-// Réseau WiFi auquel vous voulez vous connecter.
+OLED myOLED(SDA, SCL, 8);
+Adafruit_BME280 bme;
+Adafruit_CCS811 ccs;
+int BH1750address = 0x23;
 const char* ssid = "iPhone (29)";
 const char* password = "ABCDZFGH";
-
-// Ce champ contient l'adresse IP ou le nom d'hôte du serveur MQTT.
 const char* mqtt_server = "mqtt.ci-ciad.utbm.fr";
-
-
 WiFiClient espClientAir; // Initialiser la bibliothèque client wifi (connexion WiFi)
-PubSubClient clientAir(espClientAir); // Créer un objet client MQTT (connexion MQTT) 
-// Maintenant, notre client MQTT s'appelle "client". Nous devons maintenant créer une connexion à un broker.
+PubSubClient clientAir(espClientAir); // Créer un objet client MQTT (connexion MQTT)
 long lastMsg = 0;
 
-
-//---Cette fonction initialise les paramètres pour la connexion WiFi et MQTT, configure le mode d'entrée
-//et de sortie pour les broches de bouton et de LED, et démarre la communication série.
+/**@brief Cette fonction initialise les paramètres pour la connexion WiFi et MQTT, configure le mode d'entrée et de sortie pour les broches de bouton et de LED, et démarre la communication série.
+ **/
 void setup() {
-  /*
-    Setup des pin des capteurs 
-  */
-  Serial.begin(9600); // Démarre la communication série
+  Serial.begin(9600);
+
+  myOLED.begin();
+  Wire.begin();
+  Serial.begin(57600);//init Serail band rate
+  if (!bme.begin(0x76)) {
+      Serial.println("Could not find a valid BME280 sensor, check wiring!");
+      while (1);
+  }
+    if(!ccs.begin()){
+        Serial.println("Could not find a valid CCS811 sensor, check wiring!");
+        while(1);
+    }
+    while(!ccs.available());
+
   delay(2000); // Donnez-moi le temps de mettre en place le moniteur série
   Serial.println("ESP32 Button Test");
 
@@ -38,9 +41,9 @@ void setup() {
   clientAir.setCallback(callback); // définit la fonction callback comme la fonction à appeler lorsqu'un message MQTT est reçu.
 }
 
-//------Cette fonction se connecte au réseau WiFi en utilisant les paramètres de connexion fournis 
-//dans les variables ssid et password.
-void setup_wifi() {
+/** @brief Cette fonction se connecte au réseau WiFi en utilisant les paramètres de connexion fournis dans les variables ssid et password.
+**/
+ void setup_wifi() {
   delay(10);
   
   // We start by connecting to a WiFi network
@@ -50,7 +53,7 @@ void setup_wifi() {
 
   WiFi.begin(ssid, password); // //démarre la connexion Wi-Fi avec les informations de connexion (SSID et mot de passe) fournies.
 
-//Cette boucle effectue une pause de 500 millisecondes jusqu'à ce que l'ESP32 soit 
+//Cette boucle effectue une pause de 500 millisecondes jusqu'à ce que l'ESP32 soit
 //connecté au réseau Wi-Fi. Le statut de la connexion est obtenu en appelant "WiFi.status()".
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -65,9 +68,9 @@ void setup_wifi() {
 
 
 
-//-----La fonction callback est utilisée pour traiter les messages MQTT reçus par l'ESP32 et de prévoir une action.
-//Elle est appelée chaque fois qu'un nouveau message est reçu sur un Topic auquel l'ESP32 est abonné.
-void callback(char* topic, byte* message, unsigned int length) {
+/** @brief La fonction callback est utilisée pour traiter les messages MQTT reçus par l'ESP32 et de prévoir une action Elle est appelée chaque fois qu'un nouveau message est reçu sur un Topic auquel l'ESP32 est abonné.
+**/
+ void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic); // imprime le nom du Topic sur lequel le message a été publié.
   Serial.print(". Message: ");
@@ -89,16 +92,16 @@ void callback(char* topic, byte* message, unsigned int length) {
 //Si un message MQTT (variable messageTemp) est reçu sur le Topic "esp32/led", vous vérifiez si le message est soit "on" ou "off".
 //Ensuite, vous modifiez l'état de sortie de la LED physique en fonction du message "messageTemp" reçu en utilisant la fonction digitalWrite.
 
-/**
+/*
   Code à remplir
 */
 }
 
 
 
-//La fonction "reconnect()" est utilisée pour garantir la connexion MQTT entre l'ESP32 et le broker MQTT. 
-//Elle est appelée dans la boucle principale et elle se répète jusqu'à ce que la connexion soit établie avec succès.
-void reconnect() {
+/** @brief La fonction "reconnect()" est utilisée pour garantir la connexion MQTT entre l'ESP32 et le broker MQTT. Elle est appelée dans la boucle principale et elle se répète jusqu'à ce que la connexion soit établie avec succès.
+**/
+ void reconnect() {
   // Loop until we're reconnected
   while (!clientAir.connected()) {
     Serial.print("Attempting MQTT connection...");
