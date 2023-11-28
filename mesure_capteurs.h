8a63c46b0cd4e3f7ee48dd23b0b73a03f308c44f
Tom <150679896+cqptomii@users.noreply.github.com>
2023-11-28 22:03:45 +0100

    Update mesure_capteurs.h

--- mesure_capteurs.h
+++ mesure_capteurs.h
@@ -4,16 +4,15 @@
 
 #ifndef PROJETIF3B_MESUR_CAPTEURS_H
 #define PROJETIF3B_MESUR_CAPTEURS_H
-#include <OLED_I2C.h>
 #include <Wire.h>
 #include <math.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 #include <Adafruit_CCS811.h>
+#include <Adafruit_SSD1306.h>
 
-
-void UpdateOLED();
-void getBM280Data(float bmData[]);
-void getCSS811Data(float cssData);
+void UpdateOLED(float Temp,float Hum,uint16_t co2,float pression,float lux);
+void getBM280Data(float *pressure,float *temp,int *hum);
+uint16_t getCSS811Data();
 uint16_t getLuxData();
 #endif //PROJETIF3B_MESUR_CAPTEURS_H
