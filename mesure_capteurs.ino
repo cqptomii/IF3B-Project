1a55a7027b72785234cee4d569e6e488c7773cc8
Tom <150679896+cqptomii@users.noreply.github.com>
2023-11-28 22:04:42 +0100

    Update mesure_capteurs.ino

--- mesure_capteurs.ino
+++ mesure_capteurs.ino
@@ -1,15 +1,59 @@
 #include "mesure_capteurs.h"
-void UpdateOLED(){
-
+/* @brief
+*
+*/
+void UpdateOLED(float Temp,float Hum,uint16_t co2,float pression,float lux){
+    oled.clearDisplay();
+    oled.setTextSize(2);
+    oled.print("Temperature : ");
+    oled.print(Temp);
+    oled.print((char)247);
+    oled.println("C");
+    oled.print("Humidité : ");
+    oled.print(Hum);
+    oled.println("%");
+    oled.print("Pression : ");
+    oled.print(pression);
+    oled.println("pa");
+    oled.print("Luminosité : ");
+    oled.print(lux);
+    oled.println("lux");
+    oled.print("Taux Co2 : ");
+    if(co2>1000){
+      oled.println("Dangereux");
+    }
+    else{
+      oled.println("Correct");
+    }
+    oled.display();
 }
 uint16_t getCSS811Data(){
-    int BH1750address = 0x23; //setting i2c address
+    if(ccs.available()){
+      if(!ccs.readData()){ 
+        uint16_t co2=ccs.geteCo2();
+        Serial.print("CO2");
+        Serial.print(co2);
+        return co2;
+      }
+      else{
+        Serial.println("Error");
+      }
+    }
+    else{
+      Serial.println("Error ccs unavailable ");
+    }
+    return 0;
+}
+void getBM280Data(float *pressure,float *temp,int *hum){
+
+  pressure=bmp.readPressure();
+  temp=bmp.readTemperature();
+  hum=bmp.readHumidity();
+}
+uint16_t getLuxData(){
     int i=0;uint16_t val;
     byte LuxData[2];
-    Wire.beginTransmission(BH1750address);
-    Wire.write(0x10);//1lx reolution 120ms
-    Wire.endTransmission();
-
+    
     Wire.beginTransmission(BH1750address);
     Wire.requestFrom(BH1750address, 2);
     while(Wire.available()) //
@@ -23,7 +67,8 @@ uint16_t getCSS811Data(){
         val=((buff[0]<<8)|buff[1])/1.2;
         Serial.print(val,DEC);
         Serial.println("[lx]");
+        return val;
     }
-    return val;
+    return 0;
 }
 
